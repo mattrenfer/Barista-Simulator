9683297796bfbb460990668b8e5020a6d7953586
mattrenfer <mr.renfer@gmail.com>
2021-09-26 00:04:14 -0400

    Pouring Timer/Img & Timer Clears

--- script.js
+++ script.js
@@ -45,15 +45,15 @@ function yesOrNo(a) {
     para.innerHTML = "Nah? That's cool I guess.";
   }
 
-
 }
 
-// Regular Coffee
+// Brew Regular Coffee
 function coffeeChoices(a) {
   let choices = document.getElementById("coffeeChoices");
+  let pour = document.getElementById("brewPour")
   let para = document.getElementById("responsePara");
   let countdown = document.getElementById("countdown");
-  let brewTimer = 10;
+  let brewTimer = 2;
   let brewRate = 1000;
   
   if (a === "regular") {
@@ -65,18 +65,27 @@ function coffeeChoices(a) {
     choices.classList.add("hidden");
     
     // Brew Countdown
-    setInterval(function() {
+    let brewInterval = setInterval(function() {
         
+        // timer countdown
         if (brewTimer > 0) {
           brewTimer--;
           countdown.innerHTML = brewTimer;
+          console.log(brewTimer);
         }
 
+        // when the brew timer ends
         if (brewTimer === 0) {
           para.innerHTML = "DONE BREWING<br /><img src='img/coffee-pot.png' width='25%' height='25%' />";
           countdown.innerHTML = null;
+          clearInterval(brewInterval); // stops the timer
+
+          pour.classList.remove("hidden");
+          pour.classList.add("shown");
+
         }
 
+       // timer interval
     }, brewRate); 
   }
 
@@ -90,6 +99,52 @@ function coffeeChoices(a) {
 
   if (a ==="latte") {
     para.innerHTML = "You need a LATTE more skillz to make a LATTE!<br /> Learn how to brew better, Bro (GET IT?!)"
+  }
+
+}
+
+// Brew Regular Coffee
+function regularBrew(a) {
+  let pour = document.getElementById("brewPour");
+  let drink = document.getElementById("brewDrink");
+  let para = document.getElementById("responsePara");
+  let countdown = document.getElementById("countdown");
+  let pourTimer = 2;
+  let pourRate = 1000;
+  
+  if (a === "pour") {
+
+    para.innerHTML = " ";
+    countdown.innerHTML = " ";
+
+    para.innerHTML = "Pouring...";
+    countdown.innerHTML = pourTimer;
+    
+    // Brew Countdown
+    let pourInterval = setInterval(function() {
+        
+        // timer countdown
+        if (pourTimer > 0) {
+          pourTimer--;
+          countdown.innerHTML = pourTimer;
+          console.log(pourTimer);
+        }
+
+        // when the brew timer ends
+        if (pourTimer === 0) {
+          para.innerHTML = "DONE POURING<br /><img src='img/coffee-cup.png' width='25%' height='25%' />";
+          countdown.innerHTML = null;
+          clearInterval(pourInterval); // stops the timer
+
+          pour.classList.remove("shown");
+          pour.classList.add("hidden");
+
+          drink.classList.remove("hidden");
+          drink.classList.add("shown");
+        }
+
+       // timer interval
+    }, pourRate); 
   }
 
 }
