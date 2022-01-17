bc3038e6baf7ca9ead1aa7320d867a7a11301cc0
mattrenfer <mr.renfer@gmail.com>
2022-01-17 14:08:38 -0500

    Nix Vue CLI HelloWorld Component

--- barista-sim/src/App.vue
+++ barista-sim/src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/imgs/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 import Main from '@/components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Main
   }
 }
