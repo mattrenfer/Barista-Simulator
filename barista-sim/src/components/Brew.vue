13e24bc5f5d6f8bf9580e1349fb4aa1a336a30ce
mattrenfer <mr.renfer@gmail.com>
2022-01-20 14:35:28 -0500

    Placeholder Fade In Animations | Updated Variable Styles

--- barista-sim/src/components/Brew.vue
+++ barista-sim/src/components/Brew.vue
@@ -3,7 +3,7 @@
   <div v-show="!drinkCoffee">
     <div class="descriptiveText">
       <p>Brewing . . .</p>
-      <img src="@/assets/imgs/coffee-pot.png" width='30%' height='30%' />
+        <img src="@/assets/imgs/coffee-pot.png" width='30%' height='30%' class="fade-in-image" />
     </div>
     <div class="selection">
       <button class="uiButtons" @click="drinkCoffee = true">Pour coffee!</button>
@@ -14,7 +14,7 @@
     <div class="descriptiveText">
       <p v-if="drankCoffee">Congratulations! You are a level 2 Barista!</p>
       <p v-else>Brewing . . .</p>
-      <img src="@/assets/imgs/coffee-cup.png" width='15%' height='15%' />
+      <img src="@/assets/imgs/coffee-cup.png" width='15%' height='15%' class="fade-in-image" />
     </div>
     <div class="selection">
       <button class="uiButtons" @click="drankCoffee = true">Drink coffee!</button>
@@ -38,5 +38,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.fade-in-image {
+  animation: fadeIn 5s;
+  -webkit-animation: fadeIn 5s;
+  -moz-animation: fadeIn 5s;
+  -o-animation: fadeIn 5s;
+  -ms-animation: fadeIn 5s;
+}
+@keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-moz-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-webkit-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-o-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+@-ms-keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
 
 </style>
