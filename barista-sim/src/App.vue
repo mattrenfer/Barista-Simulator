1bd4e4948c258f26693c0863d6e6c9d3adb0ea7a
mattrenfer <mr.renfer@gmail.com>
2022-01-26 12:30:16 -0500

    Revert "Vue Router Install"
    This reverts commit 1db26683b9511ea63886e2f609561b17aba3819d.

--- barista-sim/src/App.vue
+++ barista-sim/src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <transition name="fadeIn" appear>
+    <div id="app">
+      <Header :currentLevel="currentLevel" />
+      <BrewChoice @levelUp="currentLevel = $event" />
     </div>
-    <router-view/>
-  </div>
+  </transition>
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+import BrewChoice from "@/components/BrewChoice.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    BrewChoice,
+  },
+  data() {
+    return {
+      currentLevel: 1,
+    };
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "@/styles/index.scss";
+
+.fadeIn-enter-active,
+.pot-leave-active {
+  transition: opacity 2.5s;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fadeIn-enter, .pot-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
 }
 </style>
