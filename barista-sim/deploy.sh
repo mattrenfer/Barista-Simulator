fcdc8d5c014b2eb5ed45b15b18833c30f337d180
mattrenfer <mr.renfer@gmail.com>
2022-01-27 23:16:24 -0500

    Deploy Config

--- barista-sim/deploy.sh
+++ barista-sim/deploy.sh
@@ -14,12 +14,12 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'New Deployment'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:mattrenfer/Barista-Simulator.git main:gh-pages
 
 cd -
