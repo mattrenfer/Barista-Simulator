6249214e1fb12589e23c3e822ac7ede9718e950a
mattrenfer <mr.renfer@gmail.com>
2022-02-06 14:04:52 -0500

    Imported Drink Data from Shared Folder

--- src/views/Brew.vue
+++ src/views/Brew.vue
@@ -27,29 +27,7 @@
 
 <script>
 import BrewTimers from "@/components/BrewTimers.vue";
-
-const coffeeData = [
-  {
-    id: 1,
-    name: "Regular",
-    reqLevel: 1, // check against the current level
-  },
-  {
-    id: 2,
-    name: "Mocha",
-    reqLevel: 2, // check against the current level
-  },
-  {
-    id: 3,
-    name: "Espresso",
-    reqLevel: 5, // check against the current level
-  },
-  {
-    id: 4,
-    name: "Latte",
-    reqLevel: 10, // check against the current level
-  },
-];
+import { coffeeData } from "@/shared/data/drinks.js";
 
 export default {
   name: "Brew",
