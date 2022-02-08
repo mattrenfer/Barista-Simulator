7b22e602a9744d3d54192f72a12a6971691d596f
mattrenfer <mr.renfer@gmail.com>
2022-02-08 17:05:11 -0500

    Reorganized Vuex Store

--- src/store/index.js
+++ src/store/index.js
@@ -3,53 +3,60 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const state = { 
+  currentLevel: 1,
+  currentTips: 0,
+  clockedInDate: '',
+  clockedInTime: '',
+  clockedIn: false,
+  currentCustomer: '',
+  currentDrink: '',
+  currentCarafe: 4,
+  currentCups: 0,
+  carafePrice: 1,
+};
+
+const mutations = {
+  levelUp(state) {
+    state.currentLevel++;
+  },
+  getTip(state, tip) {
+    state.currentTips += tip;
+  },
+  clockedInDate(state, date) {
+    state.clockedInDate = date;
+  },
+  clockedInTime(state, time) {
+    state.clockedInTime = time
+  },
+  clockedIn(state, clocked) {
+    state.clockedIn = clocked;
+  },
+  currentCustomer(state, customer) {
+    state.currentCustomer = customer;
+  },
+  currentDrink(state, drink) {
+    state.currentDrink = drink;
+  },
+  brewCoffee(state) {
+    state.currentCups += state.currentCarafe;
+  },
+  pourCoffee(state) {
+    state.currentCups--;
+  },
+  buyCarafe(state) {
+    state.currentCarafe += 2;
+    state.currentTips -= state.carafePrice;
+    state.carafePrice *= 2;
+  },
+};
+
+const actions = { };
+const getters = { };
+
 export default new Vuex.Store({
-  state: {
-    currentLevel: 1,
-    currentTips: 0,
-    clockedInDate: '',
-    clockedInTime: '',
-    clockedIn: false,
-    currentCustomer: '',
-    currentDrink: '',
-    currentCarafe: 4,
-    currentCups: 0,
-    carafePrice: 1,
-  },
-  mutations: {
-    levelUp(state) {
-      state.currentLevel++;
-    },
-    getTip(state, tip) {
-      state.currentTips += tip;
-    },
-    clockedInDate(state, date) {
-      state.clockedInDate = date;
-    },
-    clockedInTime(state, time) {
-      state.clockedInTime = time
-    },
-    clockedIn(state, clocked) {
-      state.clockedIn = clocked;
-    },
-    currentCustomer(state, customer) {
-      state.currentCustomer = customer;
-    },
-    currentDrink(state, drink) {
-      state.currentDrink = drink;
-    },
-    brewCoffee(state) {
-      state.currentCups += state.currentCarafe;
-    },
-    pourCoffee(state) {
-      state.currentCups--;
-    },
-    buyCarafe(state) {
-      state.currentCarafe += 2;
-      state.currentTips -= state.carafePrice;
-      state.carafePrice *= 2;
-    },
-  },
-  actions: {},
-  modules: {},
+  state,
+  mutations,
+  actions,
+  getters,
 });
