1b851ffa1d654445294dc4de900b453a7e7e2a4c
mattrenfer <mr.renfer@gmail.com>
2022-02-09 15:40:00 -0500

    Settings Page: Reset Progress Button

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,7 +3,9 @@
     <div class="settings">
       <h2>Settings</h2>
       <br /><br />
-      <h3>Version {{ versionNumber }}</h3><br />
+      <h3>Version {{ versionNumber }}</h3><br /><br />
+
+      <button @click="resetProgress">Reset Progress</button>
     </div>
   </transition>
 </template>
@@ -14,6 +16,12 @@ import { version } from '../../package.json';
 
 export default {
   name: "Settings",
+  methods: {
+    resetProgress() {
+      localStorage.clear();
+      location.reload();
+    }
+  },
   data() {
       return {
           versionNumber: version,
