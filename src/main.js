71b1078cff0c9dacf386e360fb0aff01bdbfd03a
mattrenfer <mr.renfer@gmail.com>
2022-02-18 23:40:59 -0500

    Animate.css Dependencies & Global Import

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import store from './store'
+import 'animate.css';
 
 Vue.config.productionTip = false
 
