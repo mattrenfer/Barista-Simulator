4d38b8f37970e6bd27387ac92ba855bee6492de2
mattrenfer <mr.renfer@gmail.com>
2022-02-19 16:52:57 -0500

    New Order View Layout (Customer | Counter | Dialogue | Screen)

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -2,33 +2,68 @@
   <v-app>
     <div class="nav-button-left">
       <router-link to="/brew">
-          <span class="nav-button-text">Brew</span><br />
-          <v-icon class="nav-arrow" dark> mdi-arrow-left-thick </v-icon>
+        <span class="nav-button-text">Brew</span><br />
+        <v-icon class="nav-arrow" dark> mdi-arrow-left-thick </v-icon>
       </router-link>
     </div>
     <div class="nav-button-right">
       <router-link to="/brew">
-          <span class="nav-button-text">Brew</span><br />
-          <v-icon class="nav-arrow" dark> mdi-arrow-right-thick </v-icon>
+        <span class="nav-button-text">Brew</span><br />
+        <v-icon class="nav-arrow" dark> mdi-arrow-right-thick </v-icon>
       </router-link>
     </div>
+
+    <img
+      id="screenImg"
+      @click="screenUp = true"
+      src="@/assets/imgs/register-small.png"
+    />
+
+    <!-- main -->
     <div class="orders">
-      <div class="descriptiveText" v-if="this.$store.state.clockedIn">
-      <!-- "Customer" component -->
-        <transition-group name="fadeIn" appear v-if="!dayDone">
-          <p :key="2">{{ currentCustomer.name }}</p>
-          <p :key="3">
-            &quot;<em>{{ currentCustomer.order }}</em
-            >&quot;
-          </p>
-          <v-icon class="customer" :key="1">mdi-account</v-icon>
-        </transition-group>
-        <!-- End "Customer" component -->
-        <!-- <div class="descriptiveText">
+      <!-- top 50 -->
+      <div class="top-50">
+        <div class="customerLine">
+          <div class="descriptiveText" v-if="this.$store.state.clockedIn">
+            <!-- "Customer" component -->
+
+            <div id="singleCustomer">
+              <transition-group name="fadeIn" appear v-if="!dayDone">
+                <p :key="1" @click="screenUp = false">
+                  {{ currentCustomer.name }}
+                </p>
+                <v-icon class="customer" :key="2">mdi-account</v-icon>
+              </transition-group>
+            </div>
+            <!-- End "Customer" component -->
+          </div>
+
+          <div class="descriptiveText" v-show="!this.$store.state.clockedIn">
+            You have no orders. Clock in first!
+          </div>
+        </div>
+
+        <div class="counter">
+          <v-card id="counter-bg" color="#1A110A" height="100%"> </v-card>
+          <!-- "Screen" component -->
+        </div>
+      </div>
+      <!-- End top 50 -->
+
+      <div class="bot-50">
+
+        <div id="dialogueArea" v-show="this.$store.state.clockedIn && screenUp===false">
+        <!-- Serve Buttons -->
+        <div class="descriptiveText">
           <p>{{ msg }}</p>
           <p>{{ serveMsg }}</p>
         </div>
         <br />
+
+        <p id="customerOrder">
+          &quot;<em>{{ currentCustomer.order }}</em
+          >&quot;
+        </p>
         <br />
         <div class="selection" v-show="!dayDone">
           <button @click="serveDrink()" v-if="!this.currentCustomer.served">
@@ -42,104 +77,102 @@
           <button @click="refuseCustomer()" v-if="!this.currentCustomer.served">
             Refuse&nbsp;<v-icon class="icon-small">mdi-hand-front-left </v-icon>
           </button>
-        </div> -->
-      </div>
+        </div>
+        </div>
 
-     <div class="descriptiveText" v-show="!this.$store.state.clockedIn">
-        You have no orders. Clock in first!
-         
-      </div>
+        <div class="screen" v-show="screenUp">
+          <v-sheet
+            color="#2a2a2a"
+            elevation="24"
+            rounded="xl"
+            width="75%"
+            class="screen"
+          >
+            <v-card color="basil" height="500" rounded="xl">
+              <v-tabs
+                v-model="tab"
+                background-color="transparent"
+                color="basil"
+                grow
+              >
+                <v-tab v-for="item in items" :key="item">
+                  {{ item }}
+                </v-tab>
+              </v-tabs>
 
-      <v-card color="#37251b" height="100"></v-card>
-      <!-- "Screen" component -->
+              <v-tabs-items v-model="tab">
+                <v-tab-item>
+                  <v-card color="basil" flat>
+                    <v-card-text><ClockIn /></v-card-text>
+                  </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                  <v-card color="basil" flat>
+                    <v-card-text>
+                      <!-- "Drink" component (old "Pour Component") -->
 
-      <v-container class="mt-16">
-        <v-row>
-          <v-col>
-            <v-sheet color="#2a2a2a" elevation="24" rounded="xl" width="75%" class="screen">
-              <v-card color="basil" height="500" rounded="xl">
-                <v-tabs
-                  v-model="tab"
-                  background-color="transparent"
-                  color="basil"
-                  grow
-                >
-                  <v-tab v-for="item in items" :key="item">
-                    {{ item }}
-                  </v-tab>
-                </v-tabs>
-
-                <v-tabs-items v-model="tab">
-                  <v-tab-item>
-                    <v-card color="basil" flat>
-                      <v-card-text><ClockIn /></v-card-text>
-                    </v-card>
-                  </v-tab-item>
-                  <v-tab-item>
-                    <v-card color="basil" flat>
-                      <v-card-text>
-                        <!-- "Drink" component (old "Pour Component") -->
-
-                        <div id="coffeeSelection" v-show="!brewCoffee">
-                          <div class="descriptiveText">
-                            <p>Checkout</p>
-                            <p>
-                              {{ msg }}
-                            </p>
-                          </div>
-                          <div
-                            v-for="drink in drinks"
-                            :key="drink.id"
-                            class="verticalButtons"
+                      <div id="coffeeSelection" v-show="!brewCoffee">
+                        <div class="descriptiveText">
+                          <p>Checkout</p>
+                          <p>
+                            {{ msg }}
+                          </p>
+                        </div>
+                        <div
+                          v-for="drink in drinks"
+                          :key="drink.id"
+                          class="verticalButtons"
+                        >
+                          <button
+                            @click="
+                              setSelectedCoffee(drink.name, drink.reqLevel)
+                            "
+                            :class="{ locked: drink.reqLevel > level }"
                           >
-                            <button
-                              @click="
-                                setSelectedCoffee(drink.name, drink.reqLevel)
-                              "
-                              :class="{ locked: drink.reqLevel > level }"
-                            >
-                              {{ drink.name }}
-                            </button>
-                          </div>
+                            {{ drink.name }}
+                          </button>
                         </div>
+                      </div>
 
-                        <div v-if="brewCoffee">
-                          <Pour
-                            :selectedCoffee="selectedCoffee"
-                            @resetIt="brewCoffee = $event"
-                          />
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-tab-item>
-                  <v-tab-item>
-                    <v-card color="basil" flat>
-                      <v-card-text><Shop /></v-card-text>
-                    </v-card>
-                  </v-tab-item>
-                </v-tabs-items>
-              </v-card>
+                      <div v-if="brewCoffee">
+                        <Pour
+                          :selectedCoffee="selectedCoffee"
+                          @resetIt="brewCoffee = $event"
+                        />
+                      </div>
+                    </v-card-text>
+                  </v-card>
+                </v-tab-item>
+                <v-tab-item>
+                  <v-card color="basil" flat>
+                    <v-card-text><Shop /></v-card-text>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs-items>
+            </v-card>
 
-              <v-system-bar dark color="indigo darken-2">
-                Tips: {{ currentTips }}
-                <v-spacer></v-spacer>
-                <v-icon>mdi-wifi-strength-4</v-icon>
-                <v-icon>mdi-signal-cellular-outline</v-icon>
-                <v-icon>mdi-battery</v-icon>
-                <span>12:30</span>
-              </v-system-bar>
-            </v-sheet> 
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <div v-if="chalkBoard" class="mt-16">
-        <img
-          src="@/assets/imgs/chalkboard-stand.png"
-          width="30%"
-          height="30%"
-        />
+            <v-system-bar dark color="indigo darken-2">
+              Tips: {{ currentTips }}
+              <v-spacer></v-spacer>
+              stuff and things
+              <v-spacer></v-spacer>
+              <v-icon>mdi-wifi-strength-4</v-icon>
+              <v-icon>mdi-signal-cellular-outline</v-icon>
+              <v-icon>mdi-battery</v-icon>
+              <span>12:30</span>
+            </v-system-bar>
+          </v-sheet>
+        </div>
       </div>
+
+      <!-- <div v-if="chalkBoard" class="mt-16">
+              <img
+                src="@/assets/imgs/chalkboard-stand.png"
+                width="30%"
+                height="30%"
+              />
+            
+            </div> -->
     </div>
   </v-app>
 </template>
@@ -161,6 +194,7 @@ export default {
   },
   data() {
     return {
+      screenUp: false,
       brewCoffee: false,
       selectedCoffee: "",
       dayDone: false,
@@ -265,6 +299,7 @@ export default {
       this.customerRandom();
     },
     customerNext() {
+      this.brewCoffee = false;
       this.customerRandom();
     },
     setSelectedCoffee(name, reqLevel) {
@@ -287,15 +322,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
 // #app {
 //   background-image: url(../assets/imgs/orders-bg.jpg) !important;
 //   background-size:auto;
 //   background-repeat: no-repeat;
 //   background-position-y: -800px;
 // }
-
 
 .v-icon.v-icon {
   font-size: 100px !important;
@@ -308,6 +340,7 @@ export default {
 .v-icon.v-icon.customer {
   position: relative; //positions customer closer to table
   top: 17px;
+  margin: -34px;
 }
 
 /* Animation Styles */
@@ -332,15 +365,14 @@ export default {
   color: #356859 !important;
 }
 
-
-
 /* Left & Right Nav Buttons */
 
 .v-icon.v-icon.nav-arrow {
   font-size: 25px !important;
 }
 
-.nav-button-left, .nav-button-right {
+.nav-button-left,
+.nav-button-right {
   position: absolute;
   top: 50%;
 }
@@ -354,15 +386,63 @@ export default {
 }
 
 .nav-button-text:hover {
-  display:inline;
+  display: inline;
 }
 
+/* Main Flexbox Structure */
 
-/* Screen Styles */
+.orders {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.top-50 {
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.bot-50 {
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.customerLine {
+  flex-grow: 0.75;
+  height: 40%;
+}
+
+.counter {
+  height: 60%;
+  flex-grow: 1;
+}
+
+#counter-bg {
+  background-image: url(../assets/imgs/counter-top-bg-2.jpg);
+  background-repeat: repeat-x;
+}
 
 .screen {
-  margin: auto;
+  height: 560px;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  align-self: flex-end;
 }
 
+#screenImg {
+  position: absolute;
+  left: 58%;
+  top: 6%;
+  z-index: 50;
+  opacity: 0.9;
+}
 
+#screenImg:hover {
+  opacity: 1;
+}
 </style>
